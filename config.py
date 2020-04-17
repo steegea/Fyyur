437f2acddd4ea3959fe9c61d7d47f156f2419172
Alex Steege <asteege@comcast.net>
2020-04-17 17:31:54 -0400

    Updated config file.

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ DEBUG = True
 #   Connects to the database
 #   Replace "*****" with your password
 #   "fyyur" is the database name
-SQLALCHEMY_DATABASE_URI = "postgres://postgres:root12345@localhost:5432/fyyur"
+SQLALCHEMY_DATABASE_URI = "postgres://postgres:*****@localhost:5432/fyyur"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
